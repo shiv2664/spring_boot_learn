4250658a36c0d9fbf9509c6de03f5487293c2152
shiv2664 <shivamsehrawat38@gmail.com>
2025-05-11 00:46:17 +0530

    Initial commit

---
+++ spring_boot_learn/src/main/kotlin/com/shivam/spring_boot_learn/SpringBootLearnApplication.kt
@@ -0,0 +1,11 @@
+package com.shivam.spring_boot_learn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootLearnApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootLearnApplication>(*args)
+}
